a17ae15b0be2207ac88c5ced73439f4e13054685
Patricia Defez <ptddds@gmail.com>
2018-11-20 18:32:56 +0100

    in progres

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,36 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  this.cards.sort(function() {
+    return Math.random() - 0.5;
+  })
+
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+ this.pairsClicked++;
+ if(firstCard == secondCard){
+   this.pairsGuessed += 1;
+   return true;
+ }else{
+   this.pairsGuessed === this.pairsGuessed;
+   return false;
+ }
+
+
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if(this.pairsGuessed == 0){
+    return false;
+  }else if(this.pairsGuessed < 8){
+    return false;
+  }else if(this.pairsGuessed == 8){
+    return true;
+  }
 };
